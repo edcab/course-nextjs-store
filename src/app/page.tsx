06b78faeeacd9f632bfc5d150898e48fc515ad76
Edwin Cabezas <ing.edwincabezas@gmail.com>
2025-03-27 19:14:05 -0500

    add Header component with navigation links and update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+"use client"
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("Hola mundo");
   return (
     <div className={styles.page}>
       <main className={styles.main}>
