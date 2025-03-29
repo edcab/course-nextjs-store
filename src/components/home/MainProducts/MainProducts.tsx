80653a4f2ed43e5f64adec40016ec41e76d09ab8
Edwin Cabezas <ing.edwincabezas@gmail.com>
2025-03-29 09:17:47 -0500

    style: add logging to MainProducts component for rendering and environment variable

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 export const MainProducts = () => {
+    console.log('MainProducts component rendered');
+    console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
     return (
         <section>
             <h1>Main Products</h1>
