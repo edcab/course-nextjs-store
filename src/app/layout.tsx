aeabbc42c0e2e724d782f1b03cf3b7c7653fd1b2
Edwin Cabezas <ing.edwincabezas@gmail.com>
2025-03-28 20:33:45 -0500

    style: add Description component with layout and styles; update RootLayout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
+import { Inter } from 'next/font/google'
+import { Header } from 'app/components/shared/Header'
+import { Footer } from 'app/components/shared/Footer'
+import 'app/sass/globals.sass'
 
-import {Header} from "app/components/shared/Header";
-import {Footer} from "app/components/shared/Footer";
-import 'app/sass/globals.sass';
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body>
-      <Header/>
+      <body className={inter.className}>
+        <Header />
         {children}
-      <Footer/>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
