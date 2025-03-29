c60980d9d8d8bf7fb934c89b690ba1723f4c9162
Edwin Cabezas <ing.edwincabezas@gmail.com>
2025-03-29 08:21:35 -0500

    style: update font to Roboto in layout; enhance Description component with image container and blur placeholder

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,15 +1,20 @@
 import styles from './Description.module.sass';
 import Image from 'next/image';
 
+const PLACEHOLDER_IMAGE = 'L4BWiGD500_K%Nbv+b#j00PBYRH?';
+
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <Image
-        src="/images/description.jpeg"
-        alt="products marketplace"
-        width={500}
-        height={300}
-      />
+      <div className={styles.Description__imageContainer}>
+        <Image
+          src="/images/description.jpeg"
+          alt="products marketplace"
+          fill
+          placeholder='blur'
+          blurDataURL={PLACEHOLDER_IMAGE}
+        />
+      </div>
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
